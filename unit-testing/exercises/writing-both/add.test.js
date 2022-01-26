2ad7ce96b791c2fd9065362f2c0176966f232e75
denrique-alvarez <denrique.alvarez@gmail.com>
2022-01-26 17:55:19 +0100

    Update add.test.js

--- unit-testing/exercises/writing-both/add.test.js
+++ unit-testing/exercises/writing-both/add.test.js
@@ -9,22 +9,24 @@
  * @param {number} [y=0] - The right number.
  * @returns {number} The sum of x and y.
  */
-const add = () => {};
+const add = (x, y) => {
+  return x + y;
+};
 
-describe('', () => {
-  describe('', () => {
-    it('', () => {
-      const expected = _;
-      const actual = _;
-      expect(_).toEqual(_);
+describe('Adds two numbers together', () => {
+  describe('Adds two integers', () => {
+    it('Adds 5 and 9', () => {
+      const expected = 14;
+      const actual = add(5, 9);
+      expect(actual).toEqual(expected);
     });
     // ...
   });
-  describe('', () => {
-    it('', () => {
-      const expected = _;
-      const actual = _;
-      expect(_).toEqual(_);
+  describe('Adds two decimal numbers', () => {
+    it('Adds 1.34 and 4.67', () => {
+      const expected = 6.01;
+      const actual = add(1.34, 4.67);
+      expect(actual).toEqual(expected);
     });
     // ...
   });
