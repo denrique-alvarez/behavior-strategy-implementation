c13d17872667ea96470163e3d0ec0bc280898275
denrique-alvarez <denrique.alvarez@gmail.com>
2022-01-29 16:25:02 +0100

    Update reverse-a-string.test.js

--- function-design/exercises/easy/reverse-a-string.test.js
+++ function-design/exercises/easy/reverse-a-string.test.js
@@ -7,15 +7,23 @@
  * @param {string} [toReverse=''] - the string to reverse
  * @returns {string} the reversed argument
  */
-const stub = () => {};
+const stub = (toReverse = '') => {
+  if (typeof toReverse !== 'string') return 'Enter a string';
+  if (toReverse.length > 20) return 'Enter a shorter string';
+  return toReverse.split('').reverse().join('');
+};
 
 /*
-  your strategy goes here
+  1. Set a parameter and its default value to a string.
+  2. Take the argument and apply split-reverse-join to reorganize the elementes. Strings and arrays can be treated similarly.
+  3. Split takes a string a divides it into the specific values given, in this case ''.
+  4. .reverse, inverts the orders of the elements in the array.
+  5. .join turns an array into a string setting the value given in between elements. In this case, empty string ''.
 */
 
 for (const solution of [
-  secretSolution,
-  // stub
+  // secretSolution,
+  stub
 ]) {
   // the main test suite for the function
   describe(solution.name + ': reverses a string', () => {
@@ -27,6 +35,21 @@ for (const solution of [
     });
     it('a string with all capital letters', () => {
       expect(solution('ASDF')).toEqual('FDSA');
+    });
+    it('Number instead of string', () => {
+      expect(solution(5)).toEqual('Enter a string');
+    });
+    it('Boolean instead of string', () => {
+      expect(solution(true)).toEqual('Enter a string');
+    });
+    it('Object instead of string', () => {
+      expect(solution(null)).toEqual('Enter a string');
+    });
+    it('NaN instead of string', () => {
+      expect(solution(NaN)).toEqual('Enter a string');
+    });
+    it('The string is longer than 20 characters', () => {
+      expect(solution('asdfasdfasdfasdfasdfasdf')).toEqual('Enter a shorter string');
     });
     // write at least 5 more tests ...
   });
