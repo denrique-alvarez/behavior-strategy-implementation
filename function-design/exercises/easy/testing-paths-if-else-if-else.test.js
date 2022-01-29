06191fdf2fab2c6744fe765fb93628f2bf150199
denrique-alvarez <denrique.alvarez@gmail.com>
2022-01-29 16:25:07 +0100

    Update testing-paths-if-else-if-else.test.js

--- function-design/exercises/easy/testing-paths-if-else-if-else.test.js
+++ function-design/exercises/easy/testing-paths-if-else-if-else.test.js
@@ -11,9 +11,17 @@
  * @param {any} val2
  * @returns {string} the values' solution
  */
-const stub = () => {};
+const stub = (val1, val2) => {
+  if (val1 === val2) return 'strictly equal';
+  if (typeof val1 === typeof val2) return 'same type';
+  else return 'totally different';
+};
 
 /*
+  1. Set two parameters without assigning default value.
+  2. Create conditional statement to compare strictly. Return correspondant message.
+  3. Create conditional statement to compare values. Return correspondant message.
+  4. Set default message if any of the conditions are not met.
 
 */
 
@@ -24,28 +32,34 @@ for (const solution of [
   describe(solution.name + ': determines how similar two values are', () => {
     describe('when values are strictly equal', () => {
       it('two identical strings -> "strictly equal"', () => {
-        expect(solution('hello', 'hello')).toEqual(_);
+        expect(solution('hello', 'hello')).toEqual('strictly equal');
       });
       it('two identical numbers -> "strictly equal"', () => {
         // 1, 1.0
+        expect(solution(1, 1)).toEqual('strictly equal');
       });
-      it('two identical booleans -> "strictly equal"', () => {});
+      it('two identical booleans -> "strictly equal"', () => {
+        expect(solution(true, true)).toEqual('strictly equal');
+      });
     });
     describe('when values have the same type', () => {
       it('two different strings -> "same type"', () => {
-        expect(_).toEqual('same type');
+        expect(solution('hello', 'bye')).toEqual('same type');
       });
       it('two different numbers -> "same type"', () => {
-        expect(_).toEqual(_);
+        expect(solution(1, 4)).toEqual('same type');
       });
-      it('two different booleans -> "same type"', () => {});
+      it('two different booleans -> "same type"', () => {
+        expect(solution(true, false)).toEqual('same type')
+      });
     });
     describe('when values are nothing alike', () => {
       it('values that are obviously different', () => {
-        _(_(null, 4))._(_);
+        expect(solution(null, 4)).toEqual('totally different');
       });
       it('values that can be confusing', () => {
         // "4" and 4
+        expect(solution('4', 4)).toEqual('totally different')
       });
     });
   });
