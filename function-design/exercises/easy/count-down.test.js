dcdf568f9fbbf49d30fc731efc23afafade27ac7
denrique-alvarez <denrique.alvarez@gmail.com>
2022-01-29 16:24:56 +0100

    Update count-down.test.js

--- function-design/exercises/easy/count-down.test.js
+++ function-design/exercises/easy/count-down.test.js
@@ -8,15 +8,27 @@
  *  start must be an integer that is greater than 0
  * @returns {number[]} an array of all numbers from `start` to 0
  */
-const stub = () => {};
+const stub = (start = 0) => {
+  if (typeof start !== 'number') return 'Enter a number';
+  if (start < 0) return 'Invalid number';
+  if (!Number.isInteger(start)) return 'Start must be integer';
+  let number = [];
+  for (let i = start; 0 <= i; i--) {
+    number.push(i);
+  }
+  return number;
+};
 
 /*
-  your strategy goes here
+ 1. Create the parameter and set the default value to zero.
+ 2. Create a veriable named 'number' which will be returned with the array.
+ 3. Iterate as many times as the start value until zero and send every value to the number array.
+ 4. Return the array with all numbers frome biggest to smallest.
 */
 
 for (const solution of [
-  secretSolution,
-  // stub
+  // secretSolution,
+  stub
 ]) {
   // the main test suite for the function
   describe(solution.name + ': counts down to 0', () => {
@@ -28,6 +40,15 @@ for (const solution of [
     });
     it('1 -> [1, 0]', () => {
       expect(solution(1)).toEqual([1, 0]);
+    });
+    it('string -> "Enter a number"', () => {
+      expect(solution('asdf')).toEqual('Enter a number');
+    });
+    it('negative -> "Invalid number"', () => {
+      expect(solution(-5)).toEqual('Invalid number');
+    });
+    it('decimal -> "Start must be integer', () => {
+      expect(solution(1.3352)).toEqual('Start must be integer');
     });
     // write at least 5 more tests ...
   });
