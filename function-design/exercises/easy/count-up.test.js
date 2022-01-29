8ab592e667a15d6cba1588306788aebf83c7e3af
denrique-alvarez <denrique.alvarez@gmail.com>
2022-01-29 16:24:59 +0100

    Update count-up.test.js

--- function-design/exercises/easy/count-up.test.js
+++ function-design/exercises/easy/count-up.test.js
@@ -8,15 +8,32 @@
  *  max must be an integer that is greater than 0
  * @returns {number[]} an array of all numbers from 0 to `max`
  */
-const stub = () => {};
+const stub = (max = 0) => {
+  if (typeof max !== 'number') return 'Enter a number';
+  if (max < 0) return 'Enter a positive number';
+  if (!Number.isInteger(max)) return 'Enter an integer';
+  if (max >= 1000000) return 'Enter a number smaller than 1000000';
+  let number = [];
+  for (let i = 0; i <= max; i++) {
+    number.push(i);
+  }
+  return number;
+};
 
 /*
-  your strategy goes here
+  1. Create a parameter and set its default value to zero.
+  2. The parameter should be equal or greater than zero.
+  3. Create a loop that will iterate a number of times equeal to the number of the value passed in the parameter.
+  4. Send each value to the number array every iteration.
+  5. Make sure the value is a number. If not, send a message.
+  6. Make sure the value is an integer. If not, send a message.
+  7. Make sure the value is a positive number. If not, send a message.
+
 */
 
 for (const solution of [
-  secretSolution,
-  // stub
+  // secretSolution,
+  stub
 ]) {
   // the main test suite for the function
   describe(solution.name + ': counts up from 0', () => {
@@ -29,6 +46,18 @@ for (const solution of [
     });
     it('1 -> [0, 1]', () => {
       expect(solution(1)).toEqual([0, 1]);
+    });
+    it('string -> "Enter a number"', () => {
+      expect(solution('asdf')).toEqual('Enter a number');
+    });
+    it('-4 -> "Enter a positive number"', () => {
+      expect(solution(-4)).toEqual('Enter a positive number');
+    });
+    it('1.5 -> "Enter an integer"', () => {
+      expect(solution(1.5)).toEqual('Enter an integer');
+    });
+    it('1000000 -> "Enter a number smaller than 1000000"', () => {
+      expect(solution(1000000)).toEqual('Enter a number smaller than 1000000');
     });
     // write at least 5 more tests ...
   });
