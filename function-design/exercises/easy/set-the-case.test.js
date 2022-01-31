24f38fc4a85e0b2fdc70999b14b437816f52e8c6
denrique-alvarez <denrique.alvarez@gmail.com>
2022-01-31 13:45:59 +0100

    Update set-the-case.test.js

--- function-design/exercises/easy/set-the-case.test.js
+++ function-design/exercises/easy/set-the-case.test.js
@@ -10,15 +10,18 @@
  *  false: set the string to upper case
  * @returns {string} the text in all lower or upper case
  */
-const stub = () => {};
+const stub = (text = '', lowerCase = true) => {
+  return lowerCase === true ? text.toLowerCase() : text.toUpperCase();
+};
 
 /*
-  your strategy goes here
+  1. Create a condition to modify the string.
+  2. Return the string depending on the result of that condition.
 */
 
 for (const solution of [
-  secretSolution,
-  // stub
+  // secretSolution,
+  stub
 ]) {
   describe(solution.name + ': sets a text to lower or upper case', () => {
     describe("the function's default parameters", () => {
@@ -32,22 +35,55 @@ for (const solution of [
     // write the tests indicated by the comments
     describe('when set to lower case', () => {
       // when the text is an empty string
-      it(_, () => {
-        expect(solution(_, _)).toEqual(_);
+      it('"" --> ""', () => {
+        expect(solution('', true)).toEqual('');
       });
       // when the text is all upper case
+      it('"ASDF" --> "asdf"', () => {
+        expect(solution('ASDF', true)).toEqual('asdf');
+      });
       // when the text is all lower case
+      it('"asdf" --> "asdf"', () => {
+        expect(solution('asdf', true)).toEqual('asdf');
+      });
       // when the text is mixed upper and lower case
+      it('"aSdF" --> "asdf"', () => {
+        expect(solution('aSdF', true)).toEqual('asdf');
+      });
       // when the text contains punctuation
+      it('"as.df" --> "as.df"', () => {
+        expect(solution('as.df', true)).toEqual('as.df');
+      });
       // when the text contains numbers
+      it('"as123df" --> "as123df"', () => {
+        expect(solution('as123df', true)).toEqual('as123df');
+      });
     });
     describe('when set to upper case', () => {
       // when the text is an empty string
+      it('"" --> ""', () => {
+        expect(solution('', false)).toEqual('');
+      });
       // when the text is all upper case
+      it('"ASDF" --> "ASDF"', () => {
+        expect(solution('ASDF', false)).toEqual('ASDF');
+      });
       // when the text is all lower case
+      it('"asdf" --> "ASDF"', () => {
+        expect(solution('asdf', false)).toEqual('ASDF');
+      });
       // when the text is mixed upper and lower case
+      it('"asDf" --> "ASDF"', () => {
+        expect(solution('asDf', false)).toEqual('ASDF');
+      });
       // when the text contains punctuation
+      it('"AS.DF" --> "AS.DF"', () => {
+        expect(solution('AS.DF', false)).toEqual('AS.DF');
+      });
       // when the text contains numbers
+      it('"as123df" --> "AS123DF"', () => {
+        expect(solution('as123df', false)).toEqual('AS123DF');
+      });
     });
   });
 }
