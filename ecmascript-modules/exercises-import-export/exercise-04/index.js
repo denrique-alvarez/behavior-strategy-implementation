814641ac3924d390bfef88117dff114e4c9aef80
denrique-alvarez <denrique.alvarez@gmail.com>
2022-01-31 20:35:30 +0100

    Update index.js

--- ecmascript-modules/exercises-import-export/exercise-04/index.js
+++ ecmascript-modules/exercises-import-export/exercise-04/index.js
@@ -4,9 +4,13 @@ debugger;
 
 // import the values from script.js to pass the assertions
 
-const test3 = months[3] === _;
+import { months, MODULES_BECAME_STANDARD_YEAR } from './script.js';
+
+const test1 = months[0] === 'Jan';
+const test2 = months[8] === 'Dec';
+const test3 = months[3] === 'Apr';
 const test4 = months[5] === 'Sep';
-const test5 = MODULES_BECAME_STANDARD_YEAR === _;
+const test5 = MODULES_BECAME_STANDARD_YEAR === 1472;
 
 console.assert(test1, 'Test 1');
 console.assert(test2, 'Test 2');
