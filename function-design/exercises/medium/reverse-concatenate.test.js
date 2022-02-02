640a02e4a7eecedf654e3639af640a8a578b17b3
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-02 17:47:56 +0100

    Update reverse-concatenate.test.js

--- function-design/exercises/medium/reverse-concatenate.test.js
+++ function-design/exercises/medium/reverse-concatenate.test.js
@@ -8,19 +8,43 @@
  * @param {string[]} arrayOfStrings - an array of strings to concatenate, in reverse order
  * @returns {string} - the array elements joined together, in reverse order
  */
-const stub = () => {};
+const stub = (arrayOfStrings = []) => {
+  let reversedCombined = arrayOfStrings.reverse().join('');
+  return reversedCombined;
+};
 
 /*
 
 */
 
 for (const solution of [
-  secretSolution,
-  // stub
+  // secretSolution,
+  stub
 ]) {
-  describe(solution.name + ': _', () => {
-    describe('_', () => {
-      it('_', () => {});
+  describe(solution.name + ': Reverses and concatenates an array of strings', () => {
+    describe('Default values', () => {
+      it('[] --> ""', () => {
+        const expected = '';
+        const actual = solution([]);
+        expect(actual).toEqual(expected);
+      });
+      it('No argument passed --> ""', () => {
+        const expected = '';
+        const actual = solution();
+        expect(actual).toEqual(expected);
+      });
+    });
+    describe('Array of strings', () => {
+      it('["hola", "chao"] --> "chaohola"', () => {
+        const expected = 'chaohola';
+        const actual = solution(["hola", "chao"]);
+        expect(actual).toEqual(expected);
+      });
+      it('["hola"] --> "hola"', () => {
+        const expected = 'hola';
+        const actual = solution(['hola']);
+        expect(actual).toEqual(expected);
+      });
     });
   });
 }
